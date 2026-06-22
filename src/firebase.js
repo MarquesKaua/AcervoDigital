666387a2aa7194db8c74aeb3b686fc9fad9b825a
Kauã Marques <kauamarquespssss@gmail.com>
2026-06-22 20:00:35 -0300

    Arquivos banco e git ignore atualizados

--- src/firebase.js
+++ src/firebase.js
@@ -16,15 +16,15 @@ import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDvzFDVXYVNULdvPfI7BatLZSNMSHkDz6M",
-  authDomain: "acervodigital-f4649.firebaseapp.com",
-  projectId: "acervodigital-f4649",
-  storageBucket: "acervodigital-f4649.firebasestorage.app",
-  messagingSenderId: "228044737693",
-  appId: "1:228044737693:web:1a0f8e315a7f51a27a9874"
-};
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
